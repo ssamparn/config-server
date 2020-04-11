d3977ea34dc45eb5f06e05a24a796e834a9178d7
samantrs <https://samantrs@git.rabobank.nl>
2020-04-11 19:36:55 +0200

    Final Commit

---
+++ src/test/java/com/cloud/cloudconfig/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
